8dfe3359b3c83bd5467cecd6610a8d8398b6f245
Jeff Jewett <jeffjewett27@gmail.com>
2025-12-19 00:16:17 -0800

    linux set proton config

--- install_linux.sh
+++ install_linux.sh
@@ -9,6 +9,7 @@ warn() { printf 'WARN: %s\n' "$*" >&2; }
 die() { printf 'ERROR: %s\n' "$*" >&2; exit 1; }
 
 pt() {
+  echo "Running protontricks with args: $@"
   if command -v protontricks >/dev/null 2>&1; then
     protontricks "$@"
   elif command -v flatpak >/dev/null 2>&1 && flatpak info com.github.Matoking.protontricks >/dev/null 2>&1; then
